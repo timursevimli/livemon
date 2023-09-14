3dcc26943d228410c0360f0e14f64ffebf5dd5f3
Timur Sevimli <svmlitimur@gmail.com>
2023-09-15 01:40:39 +0300

    Update imports and optimise

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 'use strict';
 
 const path = require('node:path');
-const beforeExec = require('./src/lib/beforeExec.js');
-const spawn = require('./src/lib/spawn.js');
-const config = require('./timon.json');
+const fs = require('node:fs');
+const { spawn, beforeExec } = require('./lib');
+const load = require('./lib/config/load.js');
 const Watcher = require('ezwatch');
 
-const boot = async () => {
-  console.log({ path: process.cwd(), argv: process.argv });
-  const file = process.argv[2];
-  if (!file) {
-    const msg = 'Enter a 2rd param! Like: "livemon server.js"\n';
-    process.stdout.write(msg);
-    process.exit(0);
+const main = async (pathName) => {
+  const fileName = process.argv[2];
+  if (!fileName) {
+    process.stdout.write('Enter a 2rd param! Like: "livemon server.js"\n');
+    process.exit(1);
   }
+  const filePath = path.join(pathName, fileName);
+  fs.accessSync(filePath);
+  const config = await load(pathName);
   await beforeExec(config);
-  const dir = process.cwd();
   const options = { ignore: { ...config.ignore }, timeout: config.delay };
-  const watcher = new Watcher(options).watch(dir);
-  const filePath = path.join(dir, file);
+  const watcher = new Watcher(options).watch(pathName);
   const restart = spawn(filePath, config);
   watcher.on('change', () => void restart());
 };
 
-module.exports = boot;
+module.exports = main;
