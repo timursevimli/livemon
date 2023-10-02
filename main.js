a32a041ead23ecde3d986ae76e1fa4b3656473d8
Timur Sevimli <svmlitimur@gmail.com>
2023-10-02 03:14:15 +0300

    Update and optimise

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 'use strict';
 
 const path = require('node:path');
-const fs = require('node:fs');
+const fsp = require('node:fs/promises');
+const Watcher = require('ezwatch');
 const { spawn, beforeExec } = require('./lib');
 const load = require('./lib/config/load.js');
-const Watcher = require('ezwatch');
 
 const main = async (pathName) => {
-  const fileName = process.argv[2];
+  const [fileName] = process.argv.slice(2);
   if (!fileName) {
     process.stdout.write('Enter a 2rd param! Like: "livemon server.js"\n');
     process.exit(1);
   }
   const filePath = path.join(pathName, fileName);
-  fs.accessSync(filePath);
+  await fsp.access(filePath);
   const config = await load(pathName);
   await beforeExec(config);
   const options = { ignore: { ...config.ignore }, timeout: config.delay };
